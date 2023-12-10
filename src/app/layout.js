aa0d9a6adaf80d80898178d67d73e56f564436b6
Meet1103 <meetmehta1103@gmail.com>
2023-12-10 21:45:07 +0530

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,25 @@
+import { DM_Sans } from "next/font/google";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Anime Vault",
+  description: "Your favorite anime, all in one place.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
